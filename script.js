7b4e907de424f589f7ace9b947857e8347b199c0
Isaac <isaacmendoza0216@gmail.com>
2024-08-06 06:58:33 -0500

    Refactoring code and adding corner cases

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const $cash = document.getElementById("cash");
-const $calculateChangeInDrawForm = document.getElementById("calculate-form");
-
-const $priceHTML = document.getElementById("price");
-const $changeDueHTML = document.getElementById("change-due");
-const $cashInDrawerHTML = document.getElementById("cid");
-
+/* cid means Cash In Drawer */
 let price = 3.26;
 let cid = [
   ["PENNY", 1.01],
@@ -18,34 +12,52 @@ let cid = [
   ["ONE HUNDRED", 100],
 ];
 
-const displayUI = (msg, statusMsg) => {
-  const message = statusMsg ? `Status: ${statusMsg}` : msg;
+const displayResult = (msg, statusMsg) => {
+  const $changeInDrawerHTML = document.getElementById("change-due");
+  const message = statusMsg ? `Status: ${statusMsg}${msg} ` : msg;
 
-  $changeDueHTML.textContent = message;
+  displayUI();
+  $changeInDrawerHTML.innerHTML = message;
 };
 
-const getCashInDrawerHTML = () => {
-  let cashInDrawerHTML = "";
+const getArrHTML = (arr) => {
+  let arrHTML = "";
 
-  cid.forEach((unitAndValue) => {
+  arr.forEach((unitAndValue) => {
     const unit = unitAndValue[0];
     const value = unitAndValue[1];
 
-    cashInDrawerHTML += `<p class="p label-drawer"><span class="drawer-unit" >${unit}: </span><span class="drawer-value">${value}</span></p>`;
+    arrHTML += `<p class="p label-drawer"><span class="drawer-unit" >${unit}: </span><span class="drawer-value">${value}</span></p>`;
   });
 
-  return cashInDrawerHTML;
+  return arrHTML;
 };
 
-$cashInDrawerHTML.innerHTML = getCashInDrawerHTML();
+const displayUI = () => {
+  const $cashInDrawerHTML = document.getElementById("cid");
+  const cidHTML = getArrHTML(cid);
 
-const getStatusMessage = (changeDue, cashInDrawer, changeInDrawer) => {
+  $cashInDrawerHTML.innerHTML = cidHTML;
+};
+
+const getStatusMessageAndUpdateCid = (
+  changeDue,
+  cashInDrawer,
+  changeInDrawer,
+  newCid
+) => {
   if (cashInDrawer < changeDue || changeInDrawer < changeDue)
     return "INSUFFICENT_FUNDS";
 
-  if (cashInDrawer === changeDue) return "CLOSED";
+  if (cashInDrawer === changeDue) {
+    cid = newCid;
+    return "CLOSED";
+  }
 
-  if (cashInDrawer > changeDue) return "OPEN";
+  if (cashInDrawer > changeDue) {
+    cid = newCid;
+    return "OPEN";
+  }
 };
 
 const getChangeDue = (price, cash) => {
@@ -64,35 +76,50 @@ const getCashInDrawer = () => {
   return cashInDrawer;
 };
 
-const getChangeInDrawer = (changeDue) => {
+const calculateChangeFromDrawer = (changeDue) => {
   let exactChange = 0;
+  let newCid = structuredClone(cid);
+
+  let changeInDrawerArr = [];
+
   const unitValues = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
 
   unitValues.forEach((currentUnit, index) => {
-    const indexUnitCid = cid.length - (1 + index);
+    const indexUnitCid = newCid.length - (1 + index);
 
     const convertToInteger = (num) => num * 1000;
     const convertToDecimal = (num) => num / 1000;
 
-    while (currentUnit <= changeDue && cid[indexUnitCid][1] > 0) {
+    while (currentUnit <= changeDue && newCid[indexUnitCid][1] > 0) {
       exactChange += currentUnit;
+
+      console.log(changeInDrawerArr[index]);
+      if (changeInDrawerArr[index]) {
+        changeInDrawerArr[index][1] += currentUnit;
+      } else {
+        const value = currentUnit;
+        const unit = newCid[indexUnitCid][0];
+
+        changeInDrawerArr.push([unit, value]);
+      }
 
       changeDue = convertToDecimal(
         convertToInteger(changeDue) - convertToInteger(currentUnit)
       );
 
-      cid[indexUnitCid][1] = convertToDecimal(
-        convertToInteger(cid[indexUnitCid][1]) - convertToInteger(currentUnit)
+      newCid[indexUnitCid][1] = convertToDecimal(
+        convertToInteger(newCid[indexUnitCid][1]) -
+          convertToInteger(currentUnit)
       );
     }
   });
 
   const changeInDrawer = Number(exactChange).toFixed(2);
 
-  return changeInDrawer;
+  return [newCid, changeInDrawerArr, changeInDrawer];
 };
 
-const validateCustomerPrice = (cash, price) => {
+const validateCustomerChange = (cash, price) => {
   if (cash < price)
     return alert("Customer does not have enough money to purchase the item");
 
@@ -103,21 +130,47 @@ const validateCustomerPrice = (cash, price) => {
 
   if (cash > price) {
     const changeDue = getChangeDue(price, cash);
-
     const cashInDrawer = getCashInDrawer();
-    const changeInDrawer = getChangeInDrawer(changeDue);
 
-    const statusMsg = getStatusMessage(changeDue, cashInDrawer, changeInDrawer);
-    const msg = "JIJIJAJA";
+    const [newCid, changeInDrawerArr, changeInDrawer] =
+      calculateChangeFromDrawer(changeDue);
 
-    displayUI(msg, statusMsg);
+    const statusMsg = getStatusMessageAndUpdateCid(
+      changeDue,
+      cashInDrawer,
+      changeInDrawer,
+      newCid
+    );
+
+    const msg =
+      statusMsg === "INSUFFICENT_FUNDS" ? "" : getArrHTML(changeInDrawerArr);
+
+    displayResult(msg, statusMsg);
   }
 };
+
+const validateCustomerInput = (cash, price) => {
+  const cashValid = Number(cash);
+  const priceValid = Number(price);
+
+  if (!cashValid || isNaN(cash)) return alert("Please input a valid cash");
+  if (!priceValid || isNaN(price)) return alert("Please input a valid price");
+
+  validateCustomerChange(cashValid, priceValid);
+};
+
+const $calculateChangeInDrawForm = document.getElementById("calculate-form");
 
 $calculateChangeInDrawForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const cash = Number($cash.value);
+  const $cash = document.getElementById("cash");
+  const cash = $cash.value;
 
-  validateCustomerPrice(cash, price);
+  const $priceInput = document.getElementById("input-price");
+  const priceToUse = $priceInput.value || price;
+
+  validateCustomerInput(cash, priceToUse);
 });
+
+displayUI();
